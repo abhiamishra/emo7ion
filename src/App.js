ba76c206bd02513782ddc695317f084121b0c266
akshitaanand <akshitaanand@users.noreply.github.com>
2022-04-02 13:44:37 -0500

    web app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Router,
+  Link,
+  Switch
+} from "react-router-dom";
+import Landing from './pages/landing';
+import Login from './pages/login';
+import Video from './pages/video';
+import Welcome from './pages/welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* < Landing /> */}
+    {/* <Router>
+            <Routes>
+            <Route exact path='/' element={< Landing />}></Route>
+            <Route exact path='/login' element={< Login />}></Route>
+            <Route exact path='/welcome' element={< Welcome />}></Route>
+            <Route exact path='/vodeo' element={< Video />}></Route>
+    </Routes>
+  </Router> */}
+      <Welcome
+      logo="./assets/logo.png"
+      startNewCall="Start new call"
+      play="./assets/play.png"
+      viewPastFeedback="View Past Feedback"
+      smile= './assets/smile.png'
+      sad="./assets/sad.png"
+    />
+  </div>
   );
 }
 
