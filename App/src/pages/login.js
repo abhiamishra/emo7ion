798d7b65008a3da9416772518590b164819f464b
akshitaanand <=>
2022-04-02 17:29:22 -0500

    app changes

--- App/src/pages/login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Login(){
-    <h1>Login</h1>
-}
-
-export default Login
--- App/src/pages/video.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Video(){
-    <h1>Video</h1>
-}
-
-export default Video
