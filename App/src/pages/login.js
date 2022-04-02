1125d41670c99f6549b3c18a85782450cc04b123
akshitaanand <akshita214@gmail.com>
2022-04-02 17:33:36 -0500

    Web App
    Landing page and layout

---
+++ App/src/pages/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Login(){
+    <h1>Login</h1>
+}
+
+export default Login
---
+++ App/src/pages/welcome.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import logo from '../assets/logo.png'
+import play from '../assets/play.png'
+import smile from '../assets/smile.png'
+import sad from '../assets/sad.png'
+
+import '../App.css';
+
+document.body.style = "background: #707EFF;";
+
+function Welcome(props) {
+    const {startNewCall, viewPastFeedback} = props;
+  
+    return (
+      <div className="container-center-horizontal" style = {{backgroundColor: '707EFF'}}>
+        <form className="welcome-pagescreen" name="form2" action="form2" method="post">
+          <img className="logo" alt = "logo" src={logo} />
+          <div className="overlap-group-container">
+            <div className="overlap-group1">
+              <div className="rectangle-1" ></div>
+              <div className="inter-normal-white-30px">
+              Start New Call
+              </div>
+              <img className="clipart2739949-1" alt = "play button" src={play} />
+            </div>
+            <div className="overlap-group">
+              <div className="rectangle-2"></div>
+              <div className="inter-normal-white-30px">
+                View Past Feedback
+              </div>
+              <img className="clipart14746-1" alt = "smile" src={smile} />
+              <img className="clipart644289-1" alt = "sad" src={sad} />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  export default Welcome
