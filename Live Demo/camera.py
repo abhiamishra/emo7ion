f7900027d8e7ee1e18c36f85f6afdf72dab9876d
spudMelf <hurleyeamon58@gmail.com>
2022-04-18 13:47:33 -0500

    Changed coursera tutorial file

--- Live Demo/camera.py
+++ Live Demo/camera.py
@@ -4,11 +4,16 @@ import numpy as np
 
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 model = FacialExpressionModel("model.json", "model_weights.h5")
-font = cv2.FONT_HERSHEY_SIMPLEX
+#font = cv2.FONT_HERSHEY_SIMPLEX
+font = cv2.FONT_HERSHEY_PLAIN
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        ## Uncoment the following to change video source. (3rd option is for the Webcam)
+        #self.video = cv2.VideoCapture('videos/presidential_debate.mp4')
+        #self.video = cv2.VideoCapture('videos/facial_exp.mkv')
+        #self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture('0')
 
     def __del__(self):
         self.video.release()
@@ -25,7 +30,7 @@ class VideoCamera(object):
             roi = cv2.resize(fc, (48, 48))
             pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.putText(fr, pred, (x+(w//3), y-5), font, 5, (0, 0, 255), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
 
         _, jpeg = cv2.imencode('.jpg', fr)
--- Live Demo/main.py
+++ Live Demo/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 def gen(camera):
     while True:
@@ -20,4 +20,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
+    #app.run(host='localhost', debug=False)
